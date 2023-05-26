36a41d8b1b7d4fb86fc2bf2a2721cd7c262598dd
Kanawatkong <124300241+Kanawatkong@users.noreply.github.com>
2023-05-26 11:35:38 -0700

    save

--- Assignment.py
+++ Assignment.py
@@ -8,6 +8,13 @@ input()
 
 
 
-
 while True:
+    pyautogui.leftClick(510,400,0,0,0)
+    pyautogui.leftClick(1200,150)
+    pyautogui.leftClick(510,400,0,0,0)
+    pyautogui.leftClick(1200,315)
+    pyautogui.leftClick(510,400,0,0,0)
+    pyautogui.leftClick(1200,480)
+    pyautogui.leftClick(510,400,0,0,0)
+    pyautogui.leftClick(1200,635)
     pyautogui.leftClick(510,400,0,0,0)
